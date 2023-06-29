888092e9acb400741d1415284c329dbaae3e45d3
Karen Shirvanyan <shivi@karens-mbp.lan>
2023-06-29 12:20:16 -0700

    Added variables

---
+++ script.js
@@ -0,0 +1,16 @@
+/* */
+// var Person1 =  window.prompt ("What is your choice r,p, or s?");
+var choiceArr = [r,p,s] 
+
+var Person1Choice
+
+var Person2Choice
+
+
+function game(person) {
+    var Person1 =  window.prompt ("What is your choice r,p, or s?");
+    var Person2 = choiceArr[Math.floor(Math.random()*choiceArr.length)];
+    if(Person1===Person)
+    if  () 
+} 
+game();
