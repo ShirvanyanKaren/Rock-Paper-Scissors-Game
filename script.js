ec784b6871e8b80b0d98b9042faa329670c06240
Karen Shirvanyan <shivi@karens-mbp.lan>
2023-06-29 13:25:55 -0700

    finished game

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-/* */
-// var Person1 =  window.prompt ("What is your choice r,p, or s?");
-var choiceArr = [r,p,s] 
+var choiceArr = ["r", "p", "s"];
+var userWins = 0;
+var computerWins = 0;
+var ties = 0;
+var playAgain = true;
 
-var Person1Choice
+while (playAgain) {
+  function playGame() {
+    var userChoice = prompt("What is your choice? Enter R for Rock, P for Paper, or S for Scissors").toUpperCase();
+    var computerChoice = choiceArr[Math.floor(Math.random() * choiceArr.length)];
 
-var Person2Choice
+    var result;
 
+    if (userChoice === computerChoice) {
+      result = "It's a tie!";
+      ties++;
+    } else if (
+      (userChoice === "R" && computerChoice === "s") ||
+      (userChoice === "P" && computerChoice === "r") ||
+      (userChoice === "S" && computerChoice === "p")
+    ) {
+      result = "You win!";
+      userWins++;
+    } else {
+      result = "You lose!";
+      computerWins++;
+    }
 
-function game(person) {
-    var Person1 =  window.prompt ("What is your choice r,p, or s?");
-    var Person2 = choiceArr[Math.floor(Math.random()*choiceArr.length)];
-    if(Person1===Person)
-    if  () 
-} 
-game();
+    alert(`You chose ${userChoice}. The computer chose ${computerChoice}. ${result}`);
+  }
+
+  alert(`Total wins: ${userWins}\nTotal ties: ${ties}\nTotal losses: ${computerWins}`);
+  playGame();
+  playAgain = confirm("Do you want to play again?");
+}
+
+alert(`Final tally:\nTotal wins: ${userWins}\nTotal ties: ${ties}\nTotal losses: ${computerWins}`);
